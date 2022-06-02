f0800ace171d7662af5cbd854043ca5b3c84fc96
Aleksandr <ushniihk@yandex.ru>
2022-06-02 21:19:14 +0300

    fix:replace map with concurrentMap, change the verification of user's name, login, email and for films too.

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -25,37 +25,35 @@ public class UserController {
 
     @PostMapping
     public User create(@RequestBody User user) throws ValidationException {
-            if (users.containsValue(user) || (!StringUtils.hasText(user.getEmail())) || (!user.getEmail().contains("@"))
-                    || (!StringUtils.hasText(user.getLogin())) || (user.getLogin().contains(" "))
-                    || user.getBirthday().isAfter(LocalDate.now())
-            ) {
-                log.debug("Oh, no. validation failed");
-                throw new ValidationException("oh, something was wrong");
-            } else {
-                if ((user.getName() == null) || user.getName().isEmpty()) {
-                    user.setName(user.getLogin());
-                }
-                user.setId(counterID);
-                users.put(user.getId(), user);
-                log.debug("added: {}", user.toString());
-                counterID++;
+        if (users.containsValue(user) || (!StringUtils.hasText(user.getEmail())) || (!user.getEmail().contains("@"))
+                || (!StringUtils.hasText(user.getLogin())) || (user.getLogin().contains(" "))
+                || user.getBirthday().isAfter(LocalDate.now())
+        ) {
+            log.debug("Oh, no. validation failed");
+            throw new ValidationException("oh, something was wrong");
+        } else {
+            if ((user.getName() == null) || user.getName().isEmpty()) {
+                user.setName(user.getLogin());
             }
-
+            user.setId(counterID);
+            users.put(user.getId(), user);
+            log.debug("added: {}", user.toString());
+            counterID++;
+        }
         return user;
     }
 
     @PutMapping
     public User update(@RequestBody User user) throws ValidationException {
-            if ((!StringUtils.hasText(user.getEmail())) || (!user.getEmail().contains("@"))
-                    || (!StringUtils.hasText(user.getLogin())) || user.getLogin().contains(" ")
-                    || user.getBirthday().isAfter(LocalDate.now()) || (user.getId() < 0)) {
-                log.debug("Oh, no. validation failed");
-                throw new ValidationException("oh, something was wrong");
-            } else {
-                users.put(user.getId(), user);
-                log.debug("updated: {}", user.toString());
-            }
-
+        if ((!StringUtils.hasText(user.getEmail())) || (!user.getEmail().contains("@"))
+                || (!StringUtils.hasText(user.getLogin())) || user.getLogin().contains(" ")
+                || user.getBirthday().isAfter(LocalDate.now()) || (user.getId() < 0)) {
+            log.debug("Oh, no. validation failed");
+            throw new ValidationException("oh, something was wrong");
+        } else {
+            users.put(user.getId(), user);
+            log.debug("updated: {}", user.toString());
+        }
         return user;
     }
 }
