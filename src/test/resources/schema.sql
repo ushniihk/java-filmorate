1e785da4143069ee74d27b92a1b759f58027e39c
Aleksandr <ushniihk@yandex.ru>
2022-07-20 00:20:00 +0300

    fix: fix getDirectorsFilms

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,11 +1,13 @@
 Drop table IF EXISTS FILM_GENRE;
 Drop table IF EXISTS FILM_LIKES;
+Drop table IF EXISTS FILMS_DIRECTORS;
 Drop table IF EXISTS FILMS;
 Drop table IF EXISTS RATING_MPA;
 Drop table IF EXISTS FRIENDS;
 Drop table IF EXISTS FRIENDSHIP;
 Drop table IF EXISTS GENRE;
 Drop table IF EXISTS USERS;
+Drop table IF EXISTS DIRECTORS;
 
 CREATE TABLE IF NOT EXISTS rating_mpa
 (
@@ -62,4 +64,17 @@ CREATE TABLE IF NOT EXISTS friends
     user_id    int,
     friend_id  int,
     friendship int
+);
+
+CREATE TABLE IF NOT EXISTS DIRECTORS
+(
+    director_id INTEGER,
+    director_name varchar(69)
+);
+
+CREATE TABLE IF NOT EXISTS FILMS_DIRECTORS
+(
+    director_id int,
+    film_id int,
+    primary key (film_id, director_id)
 );
